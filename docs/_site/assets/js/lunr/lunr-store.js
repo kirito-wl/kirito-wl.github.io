9d2e96454ed5ff37610ccb42b9d01bda32442543
Will <will.louzado@approker.com>
2023-08-04 12:54:00 +0100

    reverted to previous version

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First Post",
+        "excerpt":"First Post  ","categories": ["ramblings"],
+        "tags": [],
+        "url": "/ramblings/First-Post/",
+        "teaser": null
+      }]
